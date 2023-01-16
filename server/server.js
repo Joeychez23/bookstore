5eb49f54e41ff7dc4bfd8d27fec63c0c91c97ff2
Joseph Sanchez <josephsanchez@josephs-mbp.local>
2023-01-16 15:12:17 -0800

    more testing

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-app.get("/", (req, res) => {
+//app.use(routes);
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 // Create a new instance of an Apollo server with the GraphQL schema
